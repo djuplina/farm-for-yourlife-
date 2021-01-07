bc0d5c5055366c137c6b873642e15a8df806091d
Andrew Djuplin <andrew.djuplin@austin.utexas.edu>
2021-01-07 14:35:51 -0600

    adding isometric maps and random maps

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-import pygame, sys, time, random
+#!/usr/bin/env python3
+
+import pygame
+import sys
+import time
+import random
+import math
+import os
+from pygame import mixer
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_w,
+    K_DOWN,
+    K_s,
+    K_LEFT,
+    K_a,
+    K_RIGHT,
+    K_d,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
 
 from pygame.locals import *
 pygame.init()
@@ -6,10 +28,14 @@ pygame.display.set_caption('Farm for Your Life!')
 screen = pygame.display.set_mode((900, 900),0,32)
 display = pygame.Surface((300, 300))
 
-grass_img = pygame.image.load('grass.png').convert()
-grass_img.set_colorkey((0, 0, 0))
+curr_path = os.path.dirname(__file__)  # Where your .py file is located
+assets_path = os.path.join(curr_path, 'assets')  # The assets folder path
+grass_path = os.path.join(assets_path, 'background', 'grass')
 
-f = open('map.txt')
+grass_img = pygame.image.load(os.path.join(f"{grass_path}", "grass.png")).convert_alpha()
+#grass_img.set_colorkey((0, 0, 0))
+
+f = open('map-test.txt')
 map_data = [[int(c) for c in row] for row in f.read().split('\n')]
 f.close()
 
@@ -20,13 +46,11 @@ while True:
         for x, tile in enumerate(row):
             if tile:
                 #pygame.draw.rect(display, (255, 255, 255), pygame.Rect(x * 10, y * 10, 10, 10), 1)
-                display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5))
-                if random.randint(0, 1):
-                    display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5 - 14))
+                display.blit(grass_img, (150 + x * 8 - y * 8, 100 + x * 5 + y * 5))
 
     for event in pygame.event.get():
         if event.type == QUIT:
-            pygame.quit() 
+            pygame.quit()
             sys.exit()
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
