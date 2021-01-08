d059e8aaca9c44e40ad1b3611c355bba925d358c
Andrew Djuplin <andrew.djuplin@austin.utexas.edu>
2021-01-08 12:23:11 -0600

    not working controller stuff added

--- src/main.py
+++ src/main.py
@@ -45,6 +45,8 @@ pygame.mixer.pre_init(frequency = 44100, size = -16, channels = 1, buffer = 512)
 x_offset = 8
 y_offset = 4
 
+joysticks = []
+
 curr_path = os.path.dirname(__file__)
 assets_path = os.path.join(curr_path, 'assets')
 map_path = os.path.join(f"{assets_path}", 'map.txt')
@@ -55,9 +57,9 @@ bgm_path = os.path.join(assets_path, 'audio', 'bgm')
 icon = pygame.image.load(os.path.join(f"{assets_path}", "icon.png"))
 pygame.display.set_icon(icon)
 
-grss_img = pygame.image.load(os.path.join(f"{grass_path}", "dark-grass.png")).convert_alpha()
-dirt_img = pygame.image.load(os.path.join(f"{dirt_path}", "dark-dirt.png")).convert_alpha()
-seed_img = pygame.image.load(os.path.join(f"{seed_path}", "dark-seed.png")).convert_alpha()
+grss_img = pygame.image.load(os.path.join(f"{grass_path}", "grass.png")).convert_alpha()
+dirt_img = pygame.image.load(os.path.join(f"{dirt_path}", "dirt.png")).convert_alpha()
+seed_img = pygame.image.load(os.path.join(f"{seed_path}", "seed.png")).convert_alpha()
 bgm = soundObj = pygame.mixer.Sound(os.path.join(f"{bgm_path}", 'evening.wav'))
 
 random_map.main()
@@ -66,6 +68,10 @@ map_data = [[int(c) for c in row] for row in f.read().split('\n')]
 f.close()
 
 cursor = Cursor()
+
+for i in range(pygame.joystick.get_count()):
+    joysticks.append(pygame.joystick.Joystick(i))
+    joysticks[-1].init
 
 while True:
     display.fill((0,0,0))
@@ -91,25 +97,31 @@ while True:
             if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-            if event.key == K_LEFT:
+            if event.key == K_LEFT or event.key == K_a:
                 cursor.rect.move_ip(-x_offset, -y_offset)
-            if event.key == K_RIGHT:
+            if event.key == K_RIGHT or event.key == K_d:
                 cursor.rect.move_ip(x_offset, y_offset)
-            if event.key == K_DOWN:
+            if event.key == K_DOWN or event.key == K_s:
                 cursor.rect.move_ip(-x_offset, y_offset)
-            if event.key == K_UP:
+            if event.key == K_UP or event.key == K_w:
                 cursor.rect.move_ip(x_offset, -y_offset)
-            if event.key == K_a:
-                cursor.rect.move_ip(-x_offset, -y_offset)
-            if event.key == K_d:
-                cursor.rect.move_ip(x_offset, y_offset)
-            if event.key == K_s:
-                cursor.rect.move_ip(-x_offset, y_offset)
-            if event.key == K_w:
-                cursor.rect.move_ip(x_offset, -y_offset)
+        # if event.type == JOYHATMOTION:
+        #     if joysticks[0].get_hat(0) == (-1,0): #left
+        #         cursor.rect.move_ip(-x_offset, -y_offset)
+        #     if joysticks[0].get_hat(0) == (1,0): #right
+        #         cursor.rect.move_ip(x_offset, y_offset)
+        #     if joysticks[0].get_hat(0) == (0,1): #down
+        #         cursor.rect.move_ip(-x_offset, y_offset)
+        #     if joysticks[0].get_hat(0) == (0,-1): #up
+        #         cursor.rect.move_ip(x_offset, -y_offset)
+        # if event.type == pygame.MOUSEMOTION:
+        #     mouse_position = pygame.mouse.get_pos()
+        #     cursor.rect.move(mouse_position[0], mouse_position[1])
 
     display.blit(cursor.surf, cursor.rect)
 
     screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
     pygame.display.update()
     clock.tick(60)
+
+pygame.joystick.quit()
